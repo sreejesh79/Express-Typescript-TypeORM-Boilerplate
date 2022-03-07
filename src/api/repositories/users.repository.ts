8762dabb28c12a7480dcdf18fd7120202f190d3d
sreejesh <sreejesh@kriyatma.com>
2022-03-07 17:32:34 +0530

    minor changes

--- src/api/repositories/users.repository.ts
+++ src/api/repositories/users.repository.ts
@@ -1,13 +1,19 @@
 import { Service } from 'typedi';
+import { Repository } from 'typeorm';
 import { masterDBConnection } from '../../config/db';
 import { UsersEntity } from '../entity/users.entity';
+import { IRepository } from './base.repository';
 
 @Service()
-export class UsersRepository {
+export class UsersRepository implements IRepository {
 	constructor () {}
 
+	getRepository = (): Repository<UsersEntity> => {
+		return masterDBConnection().getRepository( UsersEntity );
+	};
+
 	public getAll = async (): Promise<UsersEntity[]> => {
-		const result: UsersEntity[] = await masterDBConnection().getRepository( UsersEntity ).find();
+		const result: UsersEntity[] = await this.getRepository().find();
 		return result;
 	};
 }
